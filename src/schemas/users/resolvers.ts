f126505e6aa3d0bdb33c571857d396fa703565fd
iamdavidzeng <15779712170@163.com>
2020-06-07 21:06:32 +0800

    Update rpc method

--- src/schemas/users/resolvers.ts
+++ src/schemas/users/resolvers.ts
@@ -1,13 +1,29 @@
 export const resolvers = {
     Query: {
         getUser: async (_, { email, phone }, { rpc }) => {
-            const userInfo = await rpc.users.get_user({
-                kwargs: { email, phone }
-            })
-            return userInfo;
+            const filters = [];
+
+            if (email) {
+                const filter = {
+                    field: "email",
+                    value: email,
+                };
+                filters.push(filter);
+            }
+            if (phone) {
+                const filter = {
+                    filed: "phone",
+                    value: phone
+                };
+                filters.push(filter);
+            }
+            const userInfo = await rpc.users.list_users({
+                kwargs: { filters }
+            });
+
+            return userInfo[0];
         }
     },
     Mutation: {
-
     }
 }
